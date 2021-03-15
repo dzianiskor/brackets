e929a2a1922eba3d47d680fd2157029ce1357bf3
Denis Korzun <dzianiskor@gmail.com>
2021-03-15 14:11:25 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let input = [...str]
+    let brackets = bracketsConfig.flat()
+    let stack = []
+    for (let bracket of input) {
+        let bracketsIndex = brackets.indexOf(bracket)
+        if (brackets[bracketsIndex] === brackets[bracketsIndex + 1]) {
+            if (stack.indexOf(bracketsIndex + 1) !== -1) {
+                stack.pop()
+                continue
+            }
+        }
+        if (bracketsIndex % 2 === 0) {
+            stack.push(bracketsIndex + 1)
+        } else {
+            if (stack.pop() !== bracketsIndex) {
+
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0
 }
+
